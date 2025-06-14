8ac7492796e509bd0b4b10e435497715f3d71380
Aitor Baragaño <aitorbf@gmail.com>
2025-06-14 10:15:03 +0200

    Chore: Initial commit

---
+++ StoriesLikeApp/StoriesLikeAppTests/StoriesLikeAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesLikeAppTests.swift
+//  StoriesLikeAppTests
+//
+//  Created by Aitor Baragaño Fernández on 14/6/25.
+//
+
+import Testing
+@testable import StoriesLikeApp
+
+struct StoriesLikeAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
